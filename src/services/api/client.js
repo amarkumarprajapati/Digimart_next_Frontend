ba039fbf483b7cdc4b2ee9e24bbbb495e79b54c4
Amar Prajapati <amarkumarprajapati@gmail.com>
2026-06-23 09:24:59 +0530

    updated baseurl

--- src/services/api/client.js
+++ src/services/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:4000/api/v1",
+  baseURL: "https://digimart-nest-backend.onrender.com/api/v1",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
